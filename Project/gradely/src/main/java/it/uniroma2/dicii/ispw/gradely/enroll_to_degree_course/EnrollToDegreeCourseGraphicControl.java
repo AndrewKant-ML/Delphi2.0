3bb3c48179d96abe7f739a37f66989e7686bc972
GVCUTV <116601281+GVCUTV@users.noreply.github.com>
2023-01-26 15:00:51 +0100

    pre merge GVC state

--- Project/gradely/src/main/java/it/uniroma2/dicii/ispw/gradely/enroll_to_degree_course/EnrollToDegreeCourseGraphicControl.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma2.dicii.ispw.gradely.enroll_to_degree_course;
-
-public class EnrollToDegreeCourseGraphicControl {
-    //{
-}
--- Project/gradely/src/main/java/it/uniroma2/dicii/ispw/gradely/insert_students_grades/InsertStudentsGradesControl.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma2.dicii.ispw.gradely.insert_students_grades;
-
-public class InsertStudentsGradesControl {
-    //{
-}
